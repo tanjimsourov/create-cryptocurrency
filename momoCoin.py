29666163f0eb64d83e95ae60f5cc9f96cccb2696
tanjimsourov <tanzim0627@icloud.com>
2025-02-13 20:54:24 +0600

    connect new node func added

--- momoCoin.py
+++ momoCoin.py
@@ -159,5 +159,22 @@ def add_transaction():
 
 
 #Decentralizing blockchain
+
+#Connecting new nodes
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No Node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are connected',
+                'total_nodes': list(blockchain.nodes)}
+
+    return jsonify(response), 201    
+
+
+
 #running app
 app.run(host = '0.0.0.0', port = 5000)
